b9eda932a262f60969f2e846887a2155f16fdfa6
Ian Duran <74118170+ianjduran@users.noreply.github.com>
2024-07-25 16:10:36 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ianjduran.github.io",
+  site: "https://ianjduran.github.io/portfolio",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
